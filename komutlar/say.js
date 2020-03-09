2a3b9d6ebb59b0fe03273b7d20e8e5bab0739e06
Glitch (hello-express) <none>
2020-03-09 10:23:39 +0000

    🍄🎗 Checkpoint ./komutlar/say.js:12688553/2443

--- komutlar/say.js
+++ komutlar/say.js
@@ -1,21 +1,18 @@
 const Discord = require('discord.js');
 
 exports.run = async (client, message, args) => {
-    let tag = "∾" // tagınız
+    let tag = "" // tagınız
     const voiceChannels = message.guild.channels.filter(c => c.type === 'voice');
     let count = 0;
     for (const [id, voiceChannel] of voiceChannels) count += voiceChannel.members.size;
 
     const embed = new Discord.RichEmbed()
-        .setColor("black")
-        .addField("<a:rain_isaretleyici:677902492515958811> *Sunucudaki üye sayısı*", message.guild.memberCount)
-        .addField("<a:rain_isaretleyici:677902492515958811> *Çevrimiçi üye sayısı*", message.guild.members.filter(m => !m.user.bot && m.user.presence.status !== "offline").size)
-        .addField("<a:rain_isaretleyici:677902492515958811> *Seslideki üye sayısı*", count)
-        .addField("<a:rain_isaretleyici:677902492515958811> *Tagdaki üye sayısı*", message.guild.members.filter(m => m.user.username.includes(tag)).size) // tagınız yoksa bu satrı silin
- .setThumbnail("https://cdn.discordapp.com/attachments/668611669705883688/675669671646593024/ezgif.com-video-to-gif_10.gif")
-  .setFooter(`So come, let's watch the rain as it's falling down..`)
-
-
+        .setColor("RANDOM")
+        .addField("Sunucudaki üye sayısı", message.guild.memberCount)
+        .addField("Çevrimiçi üye sayısı", message.guild.members.filter(m => !m.user.bot && m.user.presence.status !== "offline").size)
+        .addField("Seslideki üye sayısı", count)
+        .addField("Tagdaki üye sayısı", message.guild.members.filter(m => m.user.username.includes(tag)).size) // tagınız yoksa bu satrı silin
+        .setFooter(`${message.author.tag} tarafından istendi`, message.author.avatarURL)
     message.channel.send(embed);
 
 }
@@ -24,7 +21,7 @@ exports.conf = {
     enabled: true,
     guildOnly: false,
     aliases: ['info'],
-    permLevel: 0
+    permLevel: 0  
 };
 
 exports.help = {
