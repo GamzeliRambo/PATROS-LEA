1c67fbcb5b0b68265c890d4de30db96f61bf9ea0
Glitch (hello-express) <none>
2020-11-04 22:05:19 +0000

    😄💗 Checkpoint ./komutlar/kayıt-erkek.js:5743241/6439 ./ayarlar.json:5743241/190

--- komutlar/kayıt-erkek.js
+++ komutlar/kayıt-erkek.js
@@ -1,108 +1,37 @@
-const Discord = require("discord.js");
-const db = require("quick.db");
-const ayarlar = require("../ayarlar.json");
-
+const Discord = require('discord.js');
+const db = require("quick.db")
 exports.run = async (client, message, args) => {
-  //splashen
-
-  let erkekROL = ayarlar.erkekROL;
-  let kayıtsızROL = ayarlar.kayıtsızROL;
-  let kayıtlıROL = ayarlar.kayıtlıROL;
-  let yetkili = ayarlar.yetkiliROL;
-  let kayıtLOG = ayarlar.kayıtLOG;
-
-  if (!message.member.roles.has(yetkili))
-    return message.channel.send("Bu işlemi sadece yetkililer yapabilir");
-
-  if (!args[0]) return message.channel.send(`Bir kişiyi etiketlemelisin.`);
-
-  let kullanıcı = message.mentions.users.first();
-  if (!kullanıcı)
-    return message.channel.send(
-      `${args[0]}, kullanıcısını sunucuda bulamıyorum.`
-    );
-  if (kullanıcı.bot) return;
-
-  const kurulus = new Date().getTime() - kullanıcı.createdAt.getTime();
-  var kontrol;
-  if (kurulus < 1296000000) kontrol = "Şüpheli";
-  if (kurulus > 1296000000) kontrol = "Güvenli";
-
-  let isim = args[1];
-  if (!isim) return message.channel.send(`Üyenin ismini belirtmelisin.`);
-  if (isim.length > 16) return message.channel.send(`Daha kısa bir isim yaz.`);
-
-  let yaş = args[2];
-  if (!yaş) return message.channel.send(`Üyenin yaşını belirtmelisin.`);
-  if (yaş.length > 100)
-    return message.channel.send(`Üyenin yaşı 100'den büyük olamaz.`);
-
-  const emb = new Discord.MessageEmbed()
-    .setAuthor(client.user.username, client.user.avatarURL())
-    .setThumbnail(client.user.avatarURL())
-    .setTimestamp()
-    .setColor(`#fffff0`)
-    .setFooter(`Kayıt Başarılı`);
-  let kız = db.fetch(`kız_${message.author.id}_${message.guild.id}`);
-  let erkek = db.fetch(`erkek_${message.author.id}_${message.guild.id}`);
-  let toplam = erkek + kız;
-  message.guild.members.cache.get(kullanıcı.id).setNickname(`${isim} • ${yaş}`);
-  message.guild.members.cache.get(kullanıcı.id).addRole(erkekROL);
-  message.guild.members.cache.get(kullanıcı.id).addRole(kayıtlıROL);
-  message.guild.members.cache.get(kullanıcı.id).removeRole(kayıtsızROL);
-  message.guild.members.cache
-    .get(kullanıcı.id)
-    .send(
-      emb.setDescription(
-        `• Kaydın başarıyla ${message.author} tarafından yapıldı. \n • Sunucudaki İsmin : ${isim} • ${yaş} \n • Kurallar kanalımızı okumayı unutma!`
-      )
-    );
-  db.add(`erkek_${message.author.id}_${message.guild.id}`, "1");
-  let embed2 = new Discord.MessageEmbed()
-    .setTitle(`• Bir Kullanıcı Kayıt Oldu.`)
-    .setDescription(
-      `
-• **Kayıt Olan Kullanıcı:** ${kullanıcı} \`  { ${kullanıcı.id} }  \` 
-• **İsim Yaş:** \` ${isim} | ${yaş} \`
-• **Verilen Rol:** <@&${erkekROL}> \`  { ${erkekROL} }  \` 
-• **Bu Hesap:** \`  { ${kontrol} }  \` 
-• **Sunucumuz şu an** \` ${message.guild.members.size} \`** kişi **
-• **Kayıt eden:** ${message.author} \`  { ${message.author.id} }  \` 
-• **{ ${message.author} } Toplam kayıt sayısı =**  \` ${toplam} \` 
-
-• **{    __Toplam Erkek Kaydı =  \` ${erkek} \` Toplam Kız Kaydı= \` ${kız} \`__  }**  
-`
-    )
-    .setImage(
-      "https://i.pinimg.com/originals/af/80/39/af8039261a387be71514bb4c2e5e54b5.gif"
-    );
-
-  client.channels.cache.get(ayarlar.kayıtLOG).send(embed2);
-  let embed3 = new Discord.MessageEmbed()
-    .setTitle(`• Kayıt Başarıyla Tamamlandı!.`)
-    .setDescription(
-      `
-• **Kayıt Olan Kullanıcı:** ${kullanıcı} \`  { ${kullanıcı.id} }  \` 
-• **İsim Yaş:** \` ${isim} | ${yaş} \`
-• **Verilen Rol:** <@&${erkekROL}> \`  { ${erkekROL} }  \` 
-• **Bu Hesap:** \`  { ${kontrol} }  \` 
-• **Sunucumuz şu an**  \` ${message.guild.members.size} \`** kişi **
-• **Kayıt eden:** ${message.author} \`  { ${message.author.id} }  \` 
-`
-    )
-    .setImage(
-      "https://i.pinimg.com/originals/af/80/39/af8039261a387be71514bb4c2e5e54b5.gif"
-    );
-  message.channel.send(embed3);
-};
-
+ if (!message.member.roles.cache.has("773266328785387570") && !message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(new Discord.MessageEmbed().setDescription('Bu komutu kullanabilmek için gerekli yetkiye sahip değilsin!').setColor("Black"));
+  let kullanıcı = message.mentions.users.first()
+  if (!kullanıcı) return message.channel.send(new Discord.MessageEmbed().addField("Hatalı Kullanım",`Lütfen Bir Kullanıcı Etiketleyiniz`).setColor("RANDOM"));
+  let user = message.mentions.users.first();
+  let rol = message.mentions.roles.first()
+  let member = message.guild.member(kullanıcı)
+   let isim = args[1];
+      if(!isim) return message.channel.send(new Discord.MessageEmbed().addField("Hatalı Kullanım",`Lütfen Bir İsim Yazınız`).setColor("RANDOM")).then(m => m.delete({timeout: 5000}));
+   let yas = args[2];
+      if(!yas) return message.channel.send(new Discord.MessageEmbed().addField("Hatalı Kullanım",`Lütfen Bir Yaş Yazınız`).setColor("RANDOM")).then(m => m.delete({timeout: 5000}));
+await member.setNickname(`${isim} | ${yas}`)
+  member.roles.add("773266337396162572"); 
+  member.roles.remove("773266340387356693");
+     const kanal = message.guild.channels.cache.find(c => c.id == "773266406208307210")
+    const embed1 = new Discord.MessageEmbed() 
+    .setDescription(`<@!${member.id}> Sunucumuza Hoşgeldin Seninle Beraber \`${member.guild.memberCount}\` Kişi Olduk \n <#KURALLAR> Kanalından Kuralları Okumayı Unutma`)
+    .setColor("RANDOM")
+  let embed = new Discord.MessageEmbed() 
+  .setColor("RANDOM")
+  .setTimestamp()
+  .addField(`Kayıt İşlemi Başarılı`, `<@!${member.id}> **Adlı Kullanıcıya <@&773266337396162572> Rolünü Verdim ve İsmini**  • **İsim Yaş:** \${isim} | ${yas}\` **Olarak Düzenledim**`) 
+  .setFooter(`Komutu Kullanan Yetkili : ${message.author.username}` ,message.author.avatarURL({dynamic: true }))
+  return message.channel.send(embed).then(kanal.send(embed1)).then(m => m.delete({timeout: 20000}));
+}
 exports.conf = {
   enabled: true,
-  guildOnly: false,
-  aliases: ["e"],
+  guildOnly: true,
+  aliases: ["erkek" , "e"],
   permLevel: 0
-};
-
+}
 exports.help = {
-  name: "erkek"
-}; //splashen
+  name: 'Erkek',
+
+}
