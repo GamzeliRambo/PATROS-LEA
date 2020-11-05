86e4219373fabb1d4bc9cf0ff9bae5667f161cc5
Glitch (hello-express) <none>
2020-11-05 16:46:39 +0000

    🚨🍤 Checkpoint ./komutlar/restart.js:5743241/332 ./bot.js:5743241/333 ./komutlar/kayıt-kız.js:5743241/9077

--- komutlar/kayıt-kız.js
+++ komutlar/kayıt-kız.js
@@ -1,55 +1,48 @@
-const Discord = require("discord.js");
-const db = require("quick.db");
-const ayarlar = require("../ayarlar.json");
-var p = ayarlar.prefix
+const Discord = require('discord.js');
 
-exports.run = async(client, message, args) => { 
+exports.run = async (client, message, args) => {
+  
+  
+ if(!message.member.roles.cache.has('773266328785387570')) return message.channel.send('Bu kodu kullanmak için yeterli yetkin yok!')
+  
+  let member = message.mentions.members.first();
+  let isim = args[1]
+  let yaş = args[2]
+  let al = "773266340387356693"; ///alınacak rol idsi
+  let ver = "773266337094434816"; ///verilecek rol idsi
+  if (!member) return message.channel.send("Bir Kullanıcı Etiketle");
+  if (!isim) return message.channel.send("Bir İsim Girmelisin!");
+  member.setNickname(`Ꮙ ${isim} | ${yaş}`);
+  
+    member.roles.add(ver);
+    member.roles.remove(al);
+  
 
+  const embed = new Discord.MessageEmbed()
+    .setColor("PİNK")
+    .setTitle(":butterfly: Kayıt işlemi başarılı :butterfly:")
+    .setImage("https://media.tenor.com/images/5a8496cf0faf284d514a8cedc3f7332d/tenor.gif")
+    .setDescription(`
+**Kayıt Edilen Kullanıcı** : ${member.user.username}
+**Kayıt Eden Yetkili** : ${message.author.username}
 
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return console.log("Bu Sistem DM'de Kullanilamaz")
+**Kayıt İşleminde Verilen Rol** : 
+<@&773266337094434816>
 
-
-    if(message.member.roles.cache.has('773266328785387570')) {
-
-    let kdn = db.fetch(`kdn_${message.guild.id}`)
-    
-    let member = message.mentions.members.first();
-    let isim = args.slice(1,2).join("");
-    let yas = args.slice(2,3).join("");
-
-    const embed2 = new Discord.MessageEmbed()
-    .setAuthor(`Hatalı Komut`, client.user.displayAvatarURL())
-    .setDescription(`Örnek Kullanım: ${p}kadın @Sadist Lale 20`)
-
-
-    if (!member) return message.channel.send(embed2);
-    if (!isim) return message.channel.send(embed2);
-    if (!yas) return message.channel.send(embed2);
-    member.setNickname(`Ꮙ ${isim} | ${yas}`);
-    
-    member.roles.add(kdn) //Bunu Alt Alta Koyarak İstediğiniz Kadar Rol Verebilirsiniz Kayıt Yaparken
-    const embed = new Discord.MessageEmbed()
-    .setAuthor(`Kayıt Sistemi`, client.user.displayAvatarURL())
-    .setDescription(`${message.author.username} Adlı Yetkili Tarafından ${member}  Adlı Kişiye Kadın Rolü Verildi`)
-    message.channel.send(embed)
-    }else{
-      message.author.send("Bu Komutu Sadece **Yönetici** Yetkisi Olanlar Erişebilir!")
-    }
-
-}
+**Kayıt İşleminde Alınan Rol** :
+<@&773266340387356693>
+`)
+message.channel.send(embed)
+};
 
 exports.conf = {
   enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: 0,
-
-};
-
+  guildOnly: true,
+  aliases: ["kız" , "k"],
+  permLevel: 0
+}
 exports.help = {
-  name: 'kadın',
-  description: '',
-  usage: '',
- 
-};
+  name: 'kız',
+  description: "Kız Kayıt Sıstemı",
+  usage: 'Kız isim yaş'
+}
