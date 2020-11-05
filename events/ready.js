ae6709ef7df4abe786b0d4625b4125845d05a170
Glitch (hello-express) <none>
2020-11-05 12:53:27 +0000

    👘🌏 Checkpoint ./komutlar/modlog.js:5743241/731 ./komutlar/sil.js:5743241/1056 ./events/ready.js:5743241/3988

--- events/ready.js
+++ events/ready.js
@@ -8,10 +8,10 @@ var prefix = ayarlar.prefix;
 module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} İsmi İle Giriş Yapıldı!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Lrowsxrd :)`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Lrowsxrd`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Vexo :)`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Vexo`);
   client.user.setStatus("idle");
-  client.user.setActivity(`Lrowsxrd.`); 
+  client.user.setActivity(`Vexo.`); 
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.cache.size + ` adet kanala, ` + client.guilds.cache.size + ` adet sunucuya ve ` + client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };  
