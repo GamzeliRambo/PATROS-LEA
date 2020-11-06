7277b7c0215f9f00199835868b8eb4f2e5e89657
Glitch (hello-express) <none>
2020-11-06 05:55:26 +0000

    🚪🏁 Checkpoint ./komutlar/kulanıcı.js:5743241/6101

--- komutlar/kulanıcı.js
+++ komutlar/kulanıcı.js
@@ -1,45 +1,96 @@
-const Discord = require('discord.js');
-const ayarlar = require('../ayarlar.json');
+const Discord = require('discord.js')
+const moment = require('moment')
+const client = new Discord.Client();
 
-exports.run = (client, message, params) => {
-  if (!message.guild) {
-  const mesajsilindi = new Discord.RichEmbed()
-  const ozelmesajuyari = new Discord.RichEmbed()
-  .setColor(0xD97634)
-  .setTimestamp()
-  .setAuthor(message.author.username, message.author.avatarURL)
-  .setDescription(':fire: Üzgünüm, bunu yapamazsınız!')
-  return message.author.sendEmbed(ozelmesajuyari); }    
-  if (message.channel.type !== "group") {
-        var Durum = message.author.presence.status;
-        var Durm = (Durum == "online" ? (0x00AE86) : (Durum == "offline" ? (0x808080) : (Durum == "idle" ? (0xFFFF00) : (Durum == "dnd" ? (0xFF0000) : (0x00AE86)))))
-        var durm = (Durum == "online" ? ("Çevrimiçi") : (Durum == "offline" ? ("Çevrimdışı") : (Durum == "idle" ? ("Boşta") : (Durum == "dnd" ? ("Rahatsız Etmeyin") : ("Bilinmiyor/bulunamadı.")))))
-           const kullanicibilgimk = new Discord.RichEmbed()
-      const mesajsilindi = new Discord.RichEmbed()
-      .setThumbnail(message.author.avatarURL)
-      .setColor(0x808080)
-      .setTimestamp()
-      .addField('Kullanıcı adı;', message.author.username + '#' + message.author.discriminator)
-      .addField('ID;', message.author.id)
-      .addField('Kayıt tarihi;', message.author.createdAt)
-      .addField('Durum;', durm)
-      .addField('Şu an oynadığı oyun;', message.author.presence.game ? message.author.presence.game.name : 'Şu an oyun oynamamakta!')
-      .setFooter('Yıkık, iyi eğlenceler diler!', client.user.avatarURL)
-      console.log("Y!kullanıcı-bilgi komutu " + message.author.username + " tarafından kullanıldı.")
-      return message.channel.sendEmbed(mesajsilindi).then(msg => msg.delete(5000));
-      return message.channel.sendEmbed(kullanicibilgimk)
-  }
-};
-
+exports.run = async (bot, msg, args) => {
+  //lrowsxrd
+        let simdikitarih = moment.utc(msg.createdAt).format('DD MM YYYY');
+  //lrowsxrd
+        let user = msg.mentions.users.first() || msg.author;
+  //lrowsxrd
+        let userinfo = {};
+        userinfo.avatar= user.displayAvatarURL;
+        userinfo.id = user.id;
+        userinfo.od1 = msg.guild.members.cache.get(user.id).user.presence.game || "Oynadığı Bir Oyun Yok."
+        userinfo.status = user.presence.status.toString()
+  //lrowsxrd
+        .replace("dnd", `Rahatsız Etmeyin`)
+        .replace("online", `Çevrimiçi`)
+        .replace("idle", `Boşta`)
+        .replace("offline", `Çevrimdışı`)
+        userinfo.bot = user.bot.toString()
+        .replace("false", `Hayır`)
+        .replace("true", `Evet`)
+  //lrowsxrd
+        userinfo.sonmesaj = user.lastMessage || "Son Yazılan Mesaj Bulunamadı." || "Son Yazılan Mesaj Gösterilemedi."
+  
+        userinfo.dctarih = moment.utc(msg.guild.members.cache.get(user.id).user.createdAt).format('**YYYY** [Yılında] MMMM [Ayında] dddd [Gününde] (**DD/MM/YYYY**)')
+  //lrowsxrd
+        .replace("Monday", `**Pazartesi**`)
+        .replace("Tuesday", `**Salı**`)
+        .replace("Wednesday", `**Çarşamba**`)
+        .replace("Thursday", `**Perşembe**`)
+        .replace("Friday", `**Cuma**`)
+        .replace("Saturday", `**Cumartesi**`)
+        .replace("Sunday", `**Pazar**`)
+        .replace("January", `**Ocak**`)
+        .replace("February", `**Şubat**`)
+        .replace("March", `**Mart**`)
+        .replace("April", `**Nisan**`)
+        .replace("May", `**Mayıs**`)
+        .replace("June", `**Haziran**`)
+        .replace("July", `**Temmuz**`)
+        .replace("August", `**Ağustos**`)
+        .replace("September", `**Eylül**`)
+        .replace("October", `**Ekim**`)
+        .replace("November", `**Kasım**`)
+        .replace("December", `**Aralık**`)
+  //lrowsxrd
+        userinfo.dctarihkatilma = moment.utc(msg.guild.members.cache.get(user.id).joinedAt).format('**YYYY** [Yılında] MMMM [Ayında] dddd [Gününde] (**DD/MM/YYYY**)')
+  //lrowsxrd
+        .replace("Monday", `**Pazartesi**`)
+        .replace("Tuesday", `**Salı**`)
+        .replace("Wednesday", `**Çarşamba**`)
+        .replace("Thursday", `**Perşembe**`)
+        .replace("Friday", `**Cuma**`)
+        .replace("Saturday", `**Cumartesi**`)
+        .replace("Sunday", `**Pazar**`)
+        .replace("January", `**Ocak**`)
+        .replace("February", `**Şubat**`)
+        .replace("March", `**Mart**`)
+        .replace("April", `**Nisan**`)
+        .replace("May", `**Mayıs**`)
+        .replace("June", `**Haziran**`)
+        .replace("July", `**Temmuz**`)
+        .replace("August", `**Ağustos**`)
+        .replace("September", `**Eylül**`)
+        .replace("October", `**Ekim**`)
+        .replace("November", `**Kasım**`)
+        .replace("December", `**Aralık**`)
+  //lrowsxrd
+        const uembed = new Discord.MessageEmbed()
+        //lrowsxrd
+        .setTitle(user.tag)
+        .addField(`Şu Anda Oynadığı Oyun :`, userinfo.od1, false)
+        .addField(`Durum :`, userinfo.status, false)
+        .setColor('GREEN')
+        .addField(`Katılım Tarihi (Sunucu) :`, userinfo.dctarihkatilma, false)
+        .addField(`Katılım Tarihi (Discord) :`, userinfo.dctarih, false)
+        .addField(`Kimlik :`, userinfo.id, true)
+        .addField(`Bot Mu? :`, userinfo.bot, true)
+        .addField(`Roller :`, `${msg.guild.members.cache.get(user.id).roles.cache.filter(r => r.name !== "@everyone").map(r => r).join(' **|** ') || "**Bu Kullanıcıda Hiç Rol Bulunmuyor!**"}`, false)
+        .addField(`Son Gönderdiği Mesaj :`, userinfo.sonmesaj, false)
+        
+        msg.channel.send(uembed)
+    }
 exports.conf = {
   enabled: true,
-  guildOnly: false,
-  aliases: ['kullanıcı', 'kullanıcı bilgim', 'kbilgim'],
+  guildOnly: true,
+  aliases: ['kullanıcı-bilgim','kullanıcı-bilgi','kullanıcıbilgi'],
   permLevel: 0
 };
-
 exports.help = {
-  name: 'kullanıcı-bilgi',
-  description: 'Komutu kullanan kişi hakkında bilgi verir!',
-  usage: 'kullanıcı-bilgi'
+  name: 'kullanıcıbilgim',
+  description: 'lrows v12',
+  usage: 'kullanıcıbilgim'
 };
