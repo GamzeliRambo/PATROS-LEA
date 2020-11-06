f0269e5e846cda5c030a923a3fc3b23e95f6995a
Glitch (hello-express) <none>
2020-11-06 09:46:25 +0000

    💗🎨 Checkpoint ./events/ready.js:5743241/1736

--- events/ready.js
+++ events/ready.js
@@ -1,33 +1,13 @@
-const chalk = require('chalk');
-const moment = require('moment');
-const Discord = require('discord.js');
-const ayarlar = require('../ayarlar.json');
-
-var prefix = ayarlar.prefix;
-
-
 module.exports = client => {
-  console.log(`${client.user.username}: Şu an ` + client.channels.cache.size + ` adet kanala, ` + client.guilds.cache.size + ` adet sunucuya ve ` + client.users.cache.size + ` kullanıcıya hizmet veriliyor!`);  
-  //---------AKTİFLİK DURUMU İÇİN---------
-   client.user.setStatus("dnd");
-  //idle = BOŞTA
-  //online = ÇEVRİMİÇİ
-  //dnd = RAHATSIZ ETMEYİN
-  //---------AKTİFLİK DURUMU İÇİN---------
-  
-  
-  //---------DURUM İÇİN---------
-  client.user.setActivity(`Deneme`,{ type: 'LISTENING' });
-  //LISTENING = DİNLİYOR
+  client.user.setStatus("online");
+  //idle = boşta
+  //dnd = rahatsız etmeyin
+  //online = çevrimiçi
+  console.log(`${client.user.id}                                                                                                                                                                     `)
+  //client.user.setActivity(`${prefix}yardım | ${client.guilds.size} sunucu | ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} Kullanıcıyı`, { type: "LISTENING"});
+client.user.setActivity(`Vexo`, { type: "WATCHING"});  
+//LISTENING = DİNLİYOR
   //WATCHING = İZLİYOR
   //PLAYING = OYNUYOR 
-  //---------DURUM İÇİN---------
-  
-  
-  //---------YAYIN İÇİN---------
-  client.user.setActivity("Xountex", {
-  type: "STREAMING",
-  url: "https://www.twitch.tv/xountex"
-  });
-  //---------YAYIN İÇİN---------
+ console.log(`Bot Akitf Komutlar Yüklendi !`);
 };
