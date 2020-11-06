87b82dd990504f32cedcb23ab739490bdc3a7fc0
Glitch (hello-express) <none>
2020-11-06 06:52:40 +0000

    🏟🍃 Checkpoint ./komutlar/davet.js:5743241/27091 ./bot.js:5743241/8768

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -1,22 +1,17 @@
-const Discord = require('discord.js');
-exports.run = async (client, message, args) => {
-let guild = message.guild;
-
-guild.fetchInvites()
-.then(invites => message.channel.send(`Sunucunuzda ${invites.size} davet linki mevcut!`))
-.catch(console.error);
-
-}
-
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
+module.exports.run = async (client, message, args) => {
+    let mcdksr = client.guilds.get(args[0])
+    if (!mcdksr) return message.channel.send(`**Botun Ekli Olduģ Sunucu ID Giriniz**`)
+    mcdksr.channels.random().createInvite().then(a => message.channel.send(a.toString()))
+};
 exports.conf = {
-    enabled: true,
-    guildOnly: true,
-    aliases: [],
-    permLevel: 0
-  };
-  
+  enabled: false,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 3
+};
+
 exports.help = {
-    name: 'davetsayısı',
-    description: 'İstediğiniz kişiyi uyarır.',
-    usage: 'davet sayısı'
-  };
+  name: 'iddavet',
+}
