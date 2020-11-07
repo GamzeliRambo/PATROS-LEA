3e7a620951350812357909bdb6e592a753b2b813
Glitch (hello-express) <none>
2020-11-07 06:07:14 +0000

    🌮🚋 Checkpoint ./events/ready.js:5743241/1075

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,17 @@
-module.exports = client => {
-  client.user.setStatus("online");
-  //idle = boşta
-  //dnd = rahatsız etmeyin
-  //online = çevrimiçi
-  console.log(`${client.user.id}                                                                                                                                                                     `)
-  //client.user.setActivity(`${prefix}yardım | ${client.guilds.size} sunucu | ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} Kullanıcıyı`, { type: "LISTENING"});
-client.user.setActivity(`Vexo`, { type: "WATCHING"});  
-//LISTENING = DİNLİYOR
-  //WATCHING = İZLİYOR
-  //PLAYING = OYNUYOR 
- console.log(`Bot Akitf Komutlar Yüklendi !`);
-};
+module.exports = (client) => {
+
+var oynuyorkısımları = [
+"Prefix: !",
+"Aktila bot",
+"Kurban bayramınız mübarek olsun."
+]
+
+
+setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oynuyorkısımları.length-0+1)+0);
+       client.user.setActivity(oynuyorkısımları[random], { type: 'LISTENING' });
+        }, 2 * 3000);
+
+    console.log("Bot başarı ile giriş yaptı.")
+}
