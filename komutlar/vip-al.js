23cce3e787c1a1bd8481c9f57716cea88bf8fb10
Glitch (hello-express) <none>
2020-11-13 13:37:39 +0000

    📻😘 Checkpoint ./komutlar/vipver.js:5743241/160 ./komutlar/vip-al.js:5743241/2810

--- komutlar/vip-al.js
+++ komutlar/vip-al.js
@@ -2,39 +2,39 @@ const Discord = require('discord.js');
 const db = require('quick.db');
 
 exports.run = async(client, message, args) => {
-   if(!message.member.roles.cache.has('770775432421179475')) return message.channel.send('Bu komutu kullanabilmek için gerekli yetkiye sahip değilsin : `rôl adı`')
+   if(!message.member.roles.cache.has('773266312952152095')) return message.channel.send('Bu komutu kullanabilmek için gerekli yetkiye sahip değilsin : `rôl adı`')
    let member = message.mentions.users.first() || client.users.cache.get(args.join(' '))
    if(!member) {
        return message.channel.send('Bir kişi etiketlemelisin')
    }
-   let vip = message.guild.roles.cache.find(r => r.id === '773988069576802354')//Viprolİd Koy
+   let vip = message.guild.roles.cache.find(r => r.id === '773266334577328138')//Viprolİd Koy
 
    if(!vip) {
        return message.channel.send('Vip rolü ayarlanmamış veya rol aranırken bir hata oluştu logu kontrol et')
    }
 
-   let vipal = message.guild.member(member)
+   let vipyap = message.guild.member(member)
 
 
-   vipal.roles.add(vip)
+   vipyap.roles.remove(vip)
    let embed = new Discord.MessageEmbed()
    .setColor('Yellow')
-   .setTitle('Vip Üye Verildi')
-   .addField('Vip Üye Yapılan Kullanıcı',member)
+   .setTitle('Vip Üye Geri Alındı')
+   .addField('Vip Üyesi Alınan Kullanıcı',member)
    .addField('Komutu Kullanan Yetkili', message.author)
-    .setThumbnail('https://media.giphy.com/media/IgpsRT6FdinjrDWzNu/giphy.gif')   
- client.channels.cache.get('770735059803308093').send(embed)///LOG KANAL İD YAZMALISIN
+  .setThumbnail('https://media.giphy.com/media/IgpsRT6FdinjrDWzNu/giphy.gif') 
+  client.channels.cache.get('773266413112262706').send(embed)///LOG KANAL İD YAZMALISIN
 }
 
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: ['vipver','vip-ver'],
+    aliases: ['vipal','vip-al'],
     permLevel: 0
 };
 
 exports.help = {
-    name: 'vip-ver',
-    description: 'vip-ver',
-    usage: 'vip-ver'
+    name: 'vip-al',
+    description: 'vip-al',
+    usage: 'vip-al'
 };
